989f5de290cb4285d9e27bba438201bd7226aecb
Kripa Jayakumar <kripa.jay@gmail.com>
2021-03-01 19:57:45 +0530

    Added API call for GET avengers

--- src/components/avenger.jsx
+++ src/components/avenger.jsx
@@ -1,46 +1,36 @@
 import React, { Component } from "react";
 
 class Avenger extends Component {
-  state = {
-    avengerId: 10,
-    movies: ["First Avenger", "Infinity War", "Endgame"],
-    likeCount: this.props.likeCount,
-  };
+  state = {};
   render() {
     return (
       <div className="card" style={{ width: "18rem" }}>
-        <img
-          src="https://p1.hiclipart.com/preview/707/869/118/the-a-avengers-logo-png-clipart.jpg"
-          className="card-img-top"
-        />
+        <img src={this.props.avenger.imgUrl} className="card-img-top" />
         <div className="card-body">
-          <h5 className="card-title">Avenger Name</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
+          <h5 className="card-title">{this.props.avenger.name}</h5>
+          <p className="card-text">{this.props.avenger.birthName}</p>
           <ul>{this.showMovies()}</ul>
           <button
             className="btn btn-primary"
             onClick={() => this.likeAvenger(1)}
           >
-            Like <span className="badge bg-dark">{this.state.likeCount}</span>
+            Like{" "}
+            <span className="badge bg-dark">
+              {this.props.avenger.likeCount}
+            </span>
           </button>
         </div>
       </div>
     );
   }
-  isAvenger() {
-    return this.state.avengerId > 1000 ? "He is an Avenger" : "Not an avenger";
-  }
   showMovies() {
-    return this.state.movies.map((movie, index) => (
-      <li key={index}>{movie}</li>
-    ));
+    return (
+      this.props.avenger.movies &&
+      this.props.avenger.movies.map((movie, index) => (
+        <li key={index}>{movie}</li>
+      ))
+    );
   }
-  likeAvenger = (addCounter) => {
-    this.setState({ likeCount: this.state.likeCount + addCounter });
-  };
 }
 
 export default Avenger;
--- src/components/avengers.jsx
+++ src/components/avengers.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
+import axios from "axios";
 import Avenger from "./avenger";
 
 class Avengers extends Component {
   state = {
-    allAvengers: [
-      { id: 1, likeCount: 5 },
-      { id: 2, likeCount: 10 },
-      { id: 3, likeCount: 15 },
-    ],
+    allAvengers: [],
   };
   render() {
     return (
@@ -15,12 +12,26 @@ class Avengers extends Component {
         <div className="row">
           {this.state.allAvengers.map((avenger) => (
             <div key={avenger.id} className="col">
-              <Avenger key={avenger.id} likeCount={avenger.likeCount}/>
+              <Avenger key={avenger.id} avenger={avenger} />
             </div>
           ))}
         </div>
       </div>
     );
+  }
+  async componentDidMount() {
+    const { data } = await axios.get("http://localhost:5000/api/avengers");
+    let avengers = data.map((avenger) => {
+      return {
+        id: avenger._id,
+        imgUrl: avenger.imgUrl,
+        name: avenger.name,
+        birthName: avenger.birthName,
+        likeCount: avenger.likeCount,
+        movies: avenger.movies,
+      };
+    });
+    this.setState({ allAvengers: avengers });
   }
 }
 
