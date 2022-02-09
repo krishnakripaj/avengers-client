dbc970aaf6484c6325184ddf9d98946143b5045c
Kripa Jayakumar <kripa.jay@gmail.com>
2022-02-09 19:03:56 +0530

    Setup React Client

---
+++ src/components/avenger.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from "react"
+
+class Avenger extends Component {
+    render() {
+        return <h1>Avengers Application - Website in Progress </h1>
+    }
+}
+
+export default Avenger;
