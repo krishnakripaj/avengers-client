c434b627d40819c40b0648f0436f068f36dbb8b5
Kripa Jayakumar <kripa.jay@gmail.com>
2022-03-10 22:34:03 +0530

    Added functional compoenent for avengerdetails and handled navigation

---
+++ src/components/avengerdetails.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router";
+
+function Avengerdetails() {
+  let params = useParams();
+  let navigate = useNavigate();
+
+  function goHome() {
+    navigate("/avengers");
+  }
+  return (
+    <React.Fragment>
+      <h1>Avenger Details</h1>
+      <h2>Avenger Id: {params.avengerId} </h2>
+      <button onClick={goHome}>Go Back</button>
+    </React.Fragment>
+  );
+}
+
+export default Avengerdetails;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Avenger from "./components/avenger";
 import Avengers from "./components/avengers";
+import Avengerdetails from "./components/avengerdetails";
 import NavBar from "./components/navBar";
 import Home from "./components/home";
 import "bootstrap/dist/css/bootstrap.css";
+
 
 ReactDOM.render(
   <BrowserRouter>
@@ -14,8 +16,10 @@ ReactDOM.render(
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/avengers" element={<Avengers />} />
+        <Route exact path="/avengers/:avengerId" element={<Avengerdetails/>} />
       </Routes>
     </React.Fragment>
   </BrowserRouter>,
   document.getElementById("root")
+  
 );
